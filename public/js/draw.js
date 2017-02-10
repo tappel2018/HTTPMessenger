ce07bd19f9abc1ebcc317f15943660cacb85ddc3
Troy <tappel2018@francisparker.org>
2017-02-10 09:45:53 -0800

    Change general physics

--- public/js/draw.js
+++ public/js/draw.js
@@ -3,11 +3,17 @@ function draw(condensedRoom) {
 
   g2d.fillStyle="white";
   g2d.fillRect(0,0,500,500);
+  g2d.rectStyly
 
   //key is player name
   g2d.fillStyle="black";
   for (var i = 0; i < condensedRoom.clients.length; i++) {
     var playerData = condensedRoom.clients[i].gameData;
-    g2d.fillRect(playerData.x - 25, playerData.y -25, 50, 50);
+    g2d.translate(playerData.x, playerData.y);
+    g2d.rotate(playerData.angle);
+    g2d.fillRect(-25, -25, 50, 50);
+    g2d.rotate(-playerData.angle);
+    g2d.translate(-playerData.x, -playerData.y);
+
   }
 }
--- public/shared/physics.js
+++ public/shared/physics.js
@@ -1,6 +1,8 @@
 (function(exports){
 
-    var acceleration = 0.001;
+    var acceleration = 0.0001;
+    var maxSpeed = 0.1;
+    var turnSpeed = 0.005;
 
     //returns changed room
     exports.calculatePhysics = function(condensedRoom, dt){
@@ -8,50 +10,56 @@
       var newCondensedRoom = new CondensedRoom(condensedRoom, true);
 
       for (var i = 0; i <newCondensedRoom.clients.length; i++) {
-        var curClient =condensedRoom.clients[i];
+        var curClient =newCondensedRoom.clients[i];
         if (curClient.keyMap[87]) {
-          curClient.gameData.vy -= acceleration *dt;
-        } else if (curClient.keyMap[83]) {
-          curClient.gameData.vy += acceleration * dt;
+          curClient.gameData.vx += acceleration * dt * Math.cos(curClient.gameData.angle);
+          curClient.gameData.vy+= acceleration * dt * Math.sin(curClient.gameData.angle);
+
+        }
+        if (curClient.keyMap[83]) {
+          curClient.gameData.vx -= acceleration * dt * Math.cos(curClient.gameData.angle);
+          curClient.gameData.vy -= acceleration * dt * Math.sin(curClient.gameData.angle);
         }
 
         if (curClient.keyMap[65]) {
-          curClient.gameData.vx -= acceleration *dt;
-        } else if (curClient.keyMap[68]) {
-          curClient.gameData.vx += acceleration *dt;
+          curClient.gameData.angle -= turnSpeed *dt;
         }
 
-
-        if (curClient.gameData.vx > 0.1)
-          curClient.gameData.vx = 0.1;
-
-        if (curClient.gameData.vx < -0.1)
-          curClient.gameData.vx = -0.1;
-
-        if (curClient.gameData.vy > 0.1)
-          curClient.gameData.vy = 0.1;
-
-        if (curClient.gameData.vy < -0.1)
-          curClient.gameData.vy = -0.1;
-
-        if (curClient.gameData.x>525 && curClient.gameData.vx >0) {
-            curClient.gameData.x-=550;
+        if (curClient.keyMap[68]) {
+          curClient.gameData.angle += turnSpeed *dt;
         }
 
-        if (curClient.gameData.x<-25 && curClient.gameData.vx <0) {
-            curClient.gameData.x+=550;
+        if (curClient.gameData.vx > maxSpeed)
+          curClient.gameData.vx = maxSpeed;
+
+        if (curClient.gameData.vx < -maxSpeed)
+          curClient.gameData.vx = -maxSpeed;
+
+        if (curClient.gameData.vy > maxSpeed)
+          curClient.gameData.vy = maxSpeed;
+
+        if (curClient.gameData.vy < -maxSpeed)
+          curClient.gameData.vy = -maxSpeed;
+
+        if (curClient.gameData.x>550) {
+            curClient.gameData.x-=599;
         }
 
-        if (curClient.gameData.y>525 && curClient.gameData.vy >0) {
-            curClient.gameData.y-=550;
+        if (curClient.gameData.x<-50) {
+            curClient.gameData.x+=599;
         }
 
-        if (curClient.gameData.y<-25 && curClient.gameData.vy <0) {
-            curClient.gameData.y+=550;
+        if (curClient.gameData.y>550){
+            curClient.gameData.y-=599;
         }
 
-        curClient.gameData.x += curClient.gameData.vx *dt;
-        curClient.gameData.y += curClient.gameData.vy *dt;
+        if (curClient.gameData.y<-50) {
+            curClient.gameData.y+=599;
+        }
+
+        curClient.gameData.x += curClient.gameData.vx * dt ;
+        curClient.gameData.y += curClient.gameData.vy * dt ;
+
       }
 
       return newCondensedRoom;
@@ -68,10 +76,11 @@
     //changes client
     exports.initClient = function(client) {
       client.gameData = {};
-      client.gameData.x = 50;
-      client.gameData.y = 50;
+      client.gameData.x = 100;
+      client.gameData.y = 100;
       client.gameData.vx = 0;
       client.gameData.vy = 0;
+      client.gameData.angle = 0;
 
     };
 
